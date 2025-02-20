b649c85b4692c39f5708fc4fdfb6f1cdf33cd9ba
abhishiek2002 <kuntalabishek2002@gmail.com>
2025-02-20 13:39:44 +0530

    Some Features Done

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,13 +1,38 @@
-import './App.css'
-import conf from './conf/conf';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer"
+import {Outlet} from 'react-router-dom'
 
 function App() {
-  console.log(conf.appwriteUrl);
-  
-  
-  return (
-    <div>Appwrite</div>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (<div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+    <div className="w-full block">
+      <Header />
+      <main>
+        Todo: <Outlet />
+      </main>
+      <Footer />
+    </div>
+  </div>) : (<div></div>);
 }
 
-export default App
+export default App;

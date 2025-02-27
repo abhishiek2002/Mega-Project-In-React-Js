2bfc7f30c4586f4ec0008c08e7c61a222b4c1054
abhishiek2002 <kuntalabishek2002@gmail.com>
2025-02-27 19:03:51 +0530

    PostForm

--- 12MegaBlog/src/components/RTE.jsx
+++ 12MegaBlog/src/components/RTE.jsx
@@ -1,12 +1,56 @@
 import React from "react";
 import { Editor } from "@tinymce/tinymce-react";
-import {Controller} from 'react-hook-form'
+import { Controller } from "react-hook-form";
 
-export default function RTE({name, control, label, defaultValue = ""}) {
-
+export default function RTE({ name, control, label, defaultValue = "" }) {
   return (
     <div className="w-full">
-        
+      {/* label */}
+      {label && <label className="inline-block mb-1 pl-1">{label}</label>}
+
+      
+      <Controller name={name || "content"} />
+      control={control}
+      render
+      {({ field: { onChange } }) => (
+        <Editor
+          initialValue={defaultValue}
+          init={{
+            initialValue: defaultValue,
+            height: 500,
+            menubar: true,
+            plugins: [
+              "advlist",
+              "autolink",
+              "link",
+              "image",
+              "lists",
+              "charmap",
+              "preview",
+              "anchor",
+              "pagebreak",
+              "searchreplace",
+              "wordcount",
+              "visualblocks",
+              "visualchars",
+              "code",
+              "fullscreen",
+              "insertdatetime",
+              "media",
+              "table",
+              "emoticons",
+              "help",
+            ],
+            toolbar:
+              "undo redo | styles | bold italic | alignleft aligncenter alignright alignjustify | " +
+              "bullist numlist outdent indent | link image | print preview media fullscreen | " +
+              "forecolor backcolor emoticons | help",
+            content_style:
+              "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+          }}
+          onEditorChange={onChange}
+        />
+      )}
     </div>
   );
 }

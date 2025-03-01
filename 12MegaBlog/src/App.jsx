e69fabbf23aef27f0a44fec55b3d0e4f787598a8
abhishiek2002 <kuntalabishek2002@gmail.com>
2025-03-01 17:46:52 +0530

    App Component

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import authService from "./appwrite/auth";
 import { login, logout } from "./store/authSlice";
 import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer"
-import {Outlet} from 'react-router-dom'
+import Footer from "./components/Footer/Footer";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -22,17 +22,22 @@ function App() {
         }
       })
       .finally(() => setLoading(false));
-  }, []);
+    }, []);
 
-  return !loading ? (<div className="min-h-screen flex flex-wrap content-between bg-gray-400">
-    <div className="w-full block">
-      <Header />
-      <main>
-        Todo: <Outlet />
-      </main>
-      <Footer />
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          {/* Todo: */}
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
     </div>
-  </div>) : (<div></div>);
+  ) : (
+    <div></div>
+  );
 }
 
 export default App;

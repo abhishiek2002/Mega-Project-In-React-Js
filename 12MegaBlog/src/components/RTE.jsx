a07a4a5b0c41dee7ce5ceb4f6e7aa5272f8ac27d
abhishiek2002 <kuntalabishek2002@gmail.com>
2025-03-02 16:49:55 +0530

    Text Editor Component

--- 12MegaBlog/src/components/RTE.jsx
+++ 12MegaBlog/src/components/RTE.jsx
@@ -5,52 +5,50 @@ import { Controller } from "react-hook-form";
 export default function RTE({ name, control, label, defaultValue = "" }) {
   return (
     <div className="w-full">
-      {/* label */}
       {label && <label className="inline-block mb-1 pl-1">{label}</label>}
 
-      
-      <Controller name={name || "content"} />
-      control={control}
-      render
-      {({ field: { onChange } }) => (
-        <Editor
-          initialValue={defaultValue}
-          init={{
-            initialValue: defaultValue,
-            height: 500,
-            menubar: true,
-            plugins: [
-              "advlist",
-              "autolink",
-              "link",
-              "image",
-              "lists",
-              "charmap",
-              "preview",
-              "anchor",
-              "pagebreak",
-              "searchreplace",
-              "wordcount",
-              "visualblocks",
-              "visualchars",
-              "code",
-              "fullscreen",
-              "insertdatetime",
-              "media",
-              "table",
-              "emoticons",
-              "help",
-            ],
-            toolbar:
-              "undo redo | styles | bold italic | alignleft aligncenter alignright alignjustify | " +
-              "bullist numlist outdent indent | link image | print preview media fullscreen | " +
-              "forecolor backcolor emoticons | help",
-            content_style:
-              "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
-          }}
-          onEditorChange={onChange}
-        />
-      )}
+      <Controller
+        name={name || "content"}
+        control={control}
+        render={({ field: { onChange } }) => (
+          <Editor
+            apiKey="mo1eo4f452a4w4euapteatyduu4addf54btrpvhywj3yqlys"
+            initialValue={defaultValue}
+            init={{
+              initialValue: defaultValue,
+              height: 500,
+              menubar: true,
+              plugins: [
+                "image",
+                "advlist",
+                "autolink",
+                "lists",
+                "link",
+                "image",
+                "charmap",
+                "preview",
+                "anchor",
+                "searchreplace",
+                "visualblocks",
+                "code",
+                "fullscreen",
+                "insertdatetime",
+                "media",
+                "table",
+                "code",
+                "help",
+                "wordcount",
+                "anchor",
+              ],
+              toolbar:
+                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+              content_style:
+                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+            }}
+            onEditorChange={onChange}
+          />
+        )}
+      />
     </div>
   );
 }

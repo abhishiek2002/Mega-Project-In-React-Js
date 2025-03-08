cf8a0218abdb8e83e0dfb55d8e3f94bfecb5db07
abhishiek2002 <kuntalabishek2002@gmail.com>
2025-03-08 18:35:25 +0530

    Login Page

---
+++ 12MegaBlog/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Login } from '../components/index'
+function Logins() {
+  return (
+    <div className="py-8">
+      <Login />
+    </div>
+  )
+}
+
+export default Logins
